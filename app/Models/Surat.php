4a32a6e85e8eb1cfd8f2c4494d4ad4023aef4fb1
= <dhoriffitodiansyah27@gmail.com>
2025-09-27 10:08:08 +0700

    initial commit proyek arsip surat

---
+++ app/Models/Surat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Surat extends Model
+{
+    protected $fillable = ['nomor_surat', 'kategori_id', 'judul', 'file_path'];
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
+    }
+}
